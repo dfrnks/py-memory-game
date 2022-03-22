fdca9629c9cb78e4816a37030dbe424a5ff9f0b6
dfrnks <douglasfrancardoso@gmail.com>
2022-03-22 19:27:58 -0300

    Desaclopando o metodo do jogo com o de receber a posição

--- main.py
+++ main.py
@@ -10,6 +10,23 @@ if __name__ == "__main__":
     try:
         game = MemoryGame(4, 4)
         game.start()
+        # game.playing()
+
+        for i in [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [1, 0],
+            [1, 1],
+        ]:
+            _, o, p, _ = game.next(i[0], i[1])
+
+            print(f'--------------- Pontos: {p}')
+
+            for line in o:
+                print('  '.join(map(str, line)))
+
     except KeyboardInterrupt:
         # quit
         sys.exit()
--- src/memory_game.py
+++ src/memory_game.py
@@ -6,9 +6,10 @@ import logging
 
 
 class MemoryGame:
-    def __init__(self, width=4, height=4):
+    def __init__(self, width=4, height=4, character='*'):
         self.width = width
         self.height = height
+        self.character = character * 3
         self.result_table = []
         self.game_table = []
         self.game_table_copy = None
@@ -32,7 +33,7 @@ class MemoryGame:
             raise Exception('The number of elements must be great than 2')
 
         self.result_table = [[self.__getNewObj(el) for w in range(self.width)] for h in range(self.height)]
-        self.game_table = [['000' for w in range(self.width)] for h in range(self.height)]
+        self.game_table = [[self.character for w in range(self.width)] for h in range(self.height)]
 
     def __getNewObj(self, el):
         obj = random.randint(100, 99 + el)
@@ -47,90 +48,107 @@ class MemoryGame:
     def __cls(self):
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    def __playing(self, message=None):
+    def playing(self, message=None):
         self.print(message)
 
-        position = input("Enter the el position (ex: 12):")
+        position = input("Enter the Width Height position (ex: 12):")
 
         if len(position) > 2 or len(position) < 2:
-            self.__playing("Digit two numbers. Ex: 12")
+            self.playing("Digit two numbers. Ex: 12")
             return
 
-        w = int(position[0])
-        h = int(position[1])
+        w = int(position[0]) - 1
+        h = int(position[1]) - 1
 
-        if w == 0 or h == 0:
-            self.__playing("Positions cannot be zero")
-            return
+        try:
+            finished, table, points, right = self.next(width=w, height=h)
 
-        if w > self.width:
-            self.__playing(f"First number cannot be greater than {self.width}")
-            return
-
-        if h > self.height:
-            self.__playing(f"Second number cannot be greater than {self.height}")
-            return
-
-        h = h - 1
-        w = w - 1
-
-        if f'{w}{h}' in self.positions:
-            self.__playing("Position already played")
-            return
-
-        r = self.result_table[h][w]
-        if self.last is None:
-            self.last = [h, w]
-            self.game_table_copy = copy.deepcopy(self.game_table)
-            self.game_table[h][w] = r
-            self.positions.append(f'{w}{h}')
-        elif self.result_table[self.last[0]][self.last[1]] == r:
-            self.points += 10
-            self.game_table[h][w] = r
-            self.positions.append(f'{w}{h}')
-            self.last = None
-            self.num_errors = 0
-
-            if len(self.positions) == (self.width * self.height):
+            if finished:
                 self.print()
 
-                logging.info(f"Gaming complete, Total points: {self.points}")
+                logging.info(f"Gaming complete, Total points: {points}")
+
                 return
-        else:
-            self.num_errors += 1
-            punicao = self.num_errors ** 2
-            self.points = self.points - punicao if self.points - punicao >= 0 else 0
-            self.game_table[h][w] = r
+
+        except Exception as err:
+            self.playing(err)
+            return
+
+        if right == 0:
             self.print()
-            self.game_table = copy.deepcopy(self.game_table_copy)
-            self.game_table_copy = None
-
-            if f'{self.last[1]}{self.last[0]}' in self.positions:
-                self.positions.remove(f'{self.last[1]}{self.last[0]}')
-
-            self.last = None
-
+        elif right == 1:
+            self.print()
+        elif right == -1:
+            self.print(table=table)
             time.sleep(1)
+            self.print()
 
-        self.__playing()
+        self.playing()
 
-    def print(self, message=None):
+    def print(self, message=None, table=None):
         self.__cls()
 
         if message:
             logging.error(message)
 
         logging.info(f'-- Memory Game - Points: {self.points} --')
-        for line in self.game_table:
-            logging.info('  '.join(map(str, line)))
+        if table is None:
+            for line in self.game_table:
+                logging.info('  '.join(map(str, line)))
+        else:
+            for line in table:
+                logging.info('  '.join(map(str, line)))
 
         time.sleep(0.1)
 
     def start(self):
-
         self.__createTable()
 
-        # for line in self.result_table:
-        #     print('  '.join(map(str, line)))
+    def next(self, width: int, height: int) -> [bool, list, int, int]:
+        if width > self.width - 1:
+            raise Exception(f"Width number cannot be greater than {self.width}")
 
-        self.__playing()
+        if height > self.height - 1:
+            raise Exception(f"Height number cannot be greater than {self.height}")
+
+        if f'{width}{height}' in self.positions:
+            raise Exception("Position already played")
+
+        r = self.result_table[height][width]
+        if self.last is None:
+            self.last = [height, width]
+            self.game_table_copy = copy.deepcopy(self.game_table)
+            self.game_table[height][width] = r
+            self.positions.append(f'{width}{height}')
+            result_table = copy.deepcopy(self.game_table)
+
+            return False, result_table, self.points, 0
+        elif self.result_table[self.last[0]][self.last[1]] == r:
+            self.points += 10
+            self.game_table[height][width] = r
+            self.positions.append(f'{width}{height}')
+            self.last = None
+            self.num_errors = 0
+
+            result_table = copy.deepcopy(self.game_table)
+
+            if len(self.positions) == (self.width * self.height):
+                return True, result_table, self.points, 1
+
+            return False, result_table, self.points, 1
+
+        self.num_errors += 1
+        penalty = self.num_errors ** 2
+        self.points = self.points - penalty if self.points - penalty >= 0 else 0
+
+        self.game_table[height][width] = r
+        result_table = copy.deepcopy(self.game_table)
+        self.game_table = copy.deepcopy(self.game_table_copy)
+        self.game_table_copy = None
+
+        if f'{self.last[1]}{self.last[0]}' in self.positions:
+            self.positions.remove(f'{self.last[1]}{self.last[0]}')
+
+        self.last = None
+
+        return False, result_table, self.points, -1
