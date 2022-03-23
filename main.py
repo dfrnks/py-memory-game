0f7f722b2bea6544de4dc53426d6cb4d99d0f6f2
dfrnks <douglasfrancardoso@gmail.com>
2022-03-23 14:41:50 -0300

    Salvando no arquivo dentro da thread

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import logging
 import random
 import time
 import uuid
-import pandas as pd
 import os
 import threading
 
@@ -13,14 +12,15 @@ logging.basicConfig(format='%(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-def play(w, h, list, i, j):
+def play(w, h, file_path, i, j):
     id = str(uuid.uuid4())
     start_time = time.time()
 
-    game = MemoryGame(w, h, '0')
+    game = MemoryGame(w, h)
     game.start()
 
     winning = False
+    list = []
     while not winning:
         x = random.randint(0, w-1)
         y = random.randint(0, h-1)
@@ -39,37 +39,31 @@ def play(w, h, list, i, j):
             str(time.time())
         ])
 
-        # print("--- {} item. {} seconds. {} Pontos ---".format(i, (time.time() - start_time), pontos))
-        # for line in table:
-        #     logging.info('  '.join(map(str, line)))
-
     print("--- {}-{} item. {} seconds. {} Pontos ---".format(j, i, (time.time() - start_time), pontos))
 
-    # return pd.DataFrame(l, columns=['id', 'width', 'height', 'x', 'y', 'winning', 'acerto', 'pontos', 'time'])
+    with open(file_path, mode='a', encoding='utf-8') as myfile:
+        myfile.write('\n'.join(','.join(l) for l in list))
 
 
 if __name__ == "__main__":
     try:
         size = 10
-        file_path = f'jogos-{size}x{size}-full.csv'
+        file_path = f'jogos-{size}x{size}-full-001.csv'
 
         if not os.path.exists(file_path):
             with open(file_path, mode='a', encoding='utf-8') as myfile:
                 myfile.write('id,width,height,x,y,winning,acerto,pontos,time\n')
 
-        for j in range(100):
-            threads = [None] * 50
+        for j in range(500):
+            threads = [None] * 10
             results = []
 
             for i in range(len(threads)):
-                threads[i] = threading.Thread(target=play, args=(size, size, results, i, j,))
+                threads[i] = threading.Thread(target=play, args=(size, size, file_path, i, j,))
                 threads[i].start()
 
             for i in range(len(threads)):
-                threads[i].join(300)
-
-            with open(file_path, mode='a', encoding='utf-8') as myfile:
-                myfile.write('\n'.join(','.join(line) for line in results))
+                threads[i].join(600)
 
     except KeyboardInterrupt:
         sys.exit()
