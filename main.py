775473831994dbb17c3fd22250e1ed9c75a60c38
dfrnks <douglasfrancardoso@gmail.com>
2022-03-24 21:25:28 -0300

    Adicionando novas informações ao arquivo

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ def play(w, h, file_path, i, j):
 
     winning = False
     list = []
+    jogadas = 0
     while not winning:
         x = random.randint(0, w-1)
         y = random.randint(0, h-1)
+        jogadas += 1
 
         winning, table, pontos, acerto = game.next(x, y)
 
@@ -36,13 +38,21 @@ def play(w, h, file_path, i, j):
             str(winning),
             str(acerto),
             str(pontos),
+            str(jogadas),
+            str(int(len(game.positions))),
             str(time.time())
         ])
 
-        # if (time.time() - start_time) % 2:
+        # if (time.time() - start_time) % 5:
         #     with open(file_path, mode='a', encoding='utf-8') as myfile:
         #         myfile.write('\n'.join(','.join(l) for l in list) + '\n')
         #     list = []
+        # time.sleep(0.01)
+
+        # if jogadas % 100000 == 0:
+        #     print(f"Jogada {jogadas} - {len(game.positions)}")
+        # game.print(f"Jogada {jogadas} - {len(game.positions)}")
+        # print(f"Jogada {jogadas} - {len(game.positions)}")
 
     print("--- {}-{} item. {} seconds. {} Pontos ---".format(j, i, (time.time() - start_time), pontos))
 
@@ -52,12 +62,12 @@ def play(w, h, file_path, i, j):
 
 if __name__ == "__main__":
     try:
-        size = 8
-        file_path = f'jogos-{size}x{size}-full-001.csv'
+        size = 10
+        file_path = f'jogos-{size}x{size}-full.csv'
 
         if not os.path.exists(file_path):
             with open(file_path, mode='a', encoding='utf-8') as myfile:
-                myfile.write('id,width,height,x,y,winning,acerto,pontos,time\n')
+                myfile.write('id,width,height,x,y,winning,acerto,pontos,jogada,possicoes,time\n')
 
         # play(size, size, file_path, 0, 0)
 
