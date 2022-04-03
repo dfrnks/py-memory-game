569ae8d3b9c9dc9b516ad68a99050d2463f4f0a1
dfrnks <douglasfrancardoso@gmail.com>
2022-04-03 13:26:38 -0300

    Config menu game

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import sys
 import logging
+import warnings
+
+
+def warn(*args, **kwargs):
+    pass
+
+
+warnings.warn = warn
 
 from play import play_random
 from play import playing_manually_start
@@ -8,12 +16,28 @@ from play import play_with_network
 logging.basicConfig(format='%(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
 
-
 if __name__ == "__main__":
     try:
-        # playing_manually_start()
 
-        # play_random(4, 4, 1, 1)
-        play_with_network(1)
+        args = sys.argv
+
+        if len(args) < 2 or args[1] in ('-h', '--help'):
+            print(
+                f"Informe alguma opção:\n"
+                f"\t--help -h\n"
+                f"\t--manually -m\n"
+                f"\t--random -r\n"
+                f"\t--network -n\n"
+            )
+
+        if args[1] in ('-m', '--manually'):
+            playing_manually_start()
+
+        if args[1] in ('-r', '--random'):
+            play_random(4, 4, 1, 1)
+
+        if args[1] in ('-n', '--network'):
+            play_with_network(1)
+
     except KeyboardInterrupt:
         sys.exit()
